a0e7253c56bed6317e6aa3b925933eaa6456247a
agrim <agrimnigam21@gmail.com>
2016-03-17 23:27:04 -0700

    Renamed Test.java to RegexGrouping, what it actually demonstrates , Added coments to NOde.java

--- src/Execute/Node.java
+++ src/Execute/Node.java
@@ -1,5 +1,8 @@
 package Execute;
 
+/* Node Class for the graphs in class Graph Traversal
+ * That has the algorithms for bfs and dfs
+ */
 public class Node {
     int value;
     boolean visited;
--- src/Execute/RegexGrouping.java
+++ src/Execute/RegexGrouping.java
@@ -3,11 +3,18 @@ package Execute;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Test {
+public class RegexGrouping {
 
 	   public static void check() {
 		      // String to be scanned to find the pattern.
 		      String line = "This order was placed for QT3000! OK?";
+		      
+		      /* This pattern is going to divide the above string into three groups
+		      The Greedy quantifier .* will try to match as many elements it can before taking in atleast one digit,
+		      Hence we get a result "This order was placed for QT300" for the first group,
+		      The second group will be just the one digit which is 0
+		      The third group will be the rest of the remaining string "! OK?"
+		      */
 		      String pattern = "(.*)(\\d+)(.*)";
 
 		      // Create a Pattern object
@@ -15,6 +22,7 @@ public class Test {
 
 		      // Now create matcher object.
 		      Matcher m = r.matcher(line);
+		      
 		      if (m.find( )) {
 		         System.out.println("Found value: " + m.group(0) );
 		         System.out.println("Found value: " + m.group(1) );
